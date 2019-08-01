da4253254647e74a8cd056da0dd639fb89498187
cxiao1205 <51380924+cxiao1205@users.noreply.github.com>
2019-08-01 10:21:09 -0400

    Add files via upload

---
+++ 9_Developing Data Products.R
@@ -0,0 +1,45 @@
+# Building tools for improving the data analysis process, make data driven decisions, or for other infrastructure that supports other data products.
+# Introduce Shiny, rCharts, manipulate, googleVis
+# Prensenting data analysis, slidify, R Studio presenter.
+# Creating R packages, classes and methods, yhat.
+setwd("C:/Users/Xiao.Cheng/Desktop/R Code/Data Science Specialization_Coursera")
+
+# Shiny
+        # web application framework for R. Which is also a cornerstone of this class.
+        # allows you to create a graphical interface so that users can interact with your visualizations, models, and algorithms without needed to know R themselves. 
+        # HTML gives you the structure, CSS will give you the styple, java script will give you the interactivity
+library(shiny)
+        # a shiny project is a directory containing at least two files: ui.R for user interface, and server.R controls what your app does
+        # file -> new file -> shiny web app -> enter name of the project and choose multiple file. 
+
+# This section should be done i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
